a48b233b0277d9e0d1064149cb33494100558a5f
Roger <codinaroger2@gmail.com>
2023-01-11 14:13:14 +0100

    added tailwind css to filter component

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,5 +1,17 @@
 const Filter = () => {
-  return <div>Filter</div>;
+  return (
+    <div className="flex items-center justify-end gap-2">
+      <span className="border-full rounded-md bg-blue-700 p-1 text-white">
+        100
+      </span>
+      <input
+        autoComplete="off"
+        type="search"
+        className="w-100 mr-2 rounded-lg border border-slate-200 bg-gray-100 p-2"
+        placeholder="Filter podcasts..."
+      />
+    </div>
+  );
 };
 
 export default Filter;
